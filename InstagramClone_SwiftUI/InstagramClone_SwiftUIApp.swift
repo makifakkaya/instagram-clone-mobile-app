670064252620c5ee673c084651ebf81b6385483e
Mehmet Akif AKKAYA <60367291+makifakkaya@users.noreply.github.com>
2022-12-06 17:26:17 +0300

    Initial Commit

---
+++ InstagramClone_SwiftUI/InstagramClone_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramClone_SwiftUIApp.swift
+//  InstagramClone_SwiftUI
+//
+//  Created by Akif Akkaya on 6.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct InstagramClone_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
